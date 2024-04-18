8d0fa52d925d6804a39bed22ef95ce3ece5c8f49
Rhuan Levy <rhuan_os@hotmail.com>
2024-04-18 15:08:16 -0300

    chore: change header title

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,5 +1,11 @@
 <template>
   <v-app-bar :elevation="2" color="grey-darken-4 text-center">
-    <v-app-bar-title>Lanches App</v-app-bar-title>
+    <v-app-bar-title class="headerTitle">O que pedir?</v-app-bar-title>
   </v-app-bar>
 </template>
+
+<style>
+  .headerTitle {
+    font-size: 28px !important;
+  }
+</style>
