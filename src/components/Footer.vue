93f075e5ceefa8f9c2a45853d974ba7ac9b3d7a4
Rhuan Levy <rhuan_os@hotmail.com>
2024-09-04 22:45:05 -0300

    chore: unmock email

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,7 +1,7 @@
 <template>
   <v-footer class="footer" color="#212121">
     <div class="px-4 py-2 text-center w-100">
-      <p>Dúvidas, reclamações ou sugestões? Contato via email: email@email.com</p>
+      <p>Dúvidas, reclamações ou sugestões? Contato via email: <a href="mailto:oquepedirapp@gmail.com">oquepedirapp@gmail.com</a></p>
       {{ new Date().getFullYear() }} — <strong>Desenvolvido por Rhuan Levy</strong>
     </div>
   </v-footer>
@@ -11,5 +11,11 @@
 .footer {
   padding: 0 !important;
   flex: 0 !important;
+}
+
+a {
+  text-decoration: none;
+  color: #fff;
+  font-weight: 700;
 }
 </style>
