dc95d2a8974cbe024103fbd9ece78b38b8f5f1b3
Rhuan Levy <rhuan_os@hotmail.com>
2025-02-24 01:34:47 -0300

    fix routing

--- components/PageSelect.vue
+++ components/PageSelect.vue
@@ -3,8 +3,7 @@ import { ref } from 'vue';
 
 const props = defineProps<{
   pageValue: number,
-  color: string,
-  route: string
+  color: string
 }>();
 
 const tab = ref(props.pageValue);
@@ -17,13 +16,13 @@ console.log(tab.value)
   <v-container>
     <v-row justify="center" class="options ga-5">
       <v-tabs v-model="tab" bg-color="#212121" :color="color">
-        <a :href="route">
+        <a href="?tab=restaurantes">
           <v-tab :value="2">
             <Icon name="mdi:restaurant" class="mr-2" style="transform: scale(1.5);" />
             Restaurantes
           </v-tab>
         </a>
-        <a :href="route">
+        <a href="/?tab=turismo">
           <v-tab :value="1">
             <Icon name="fa-solid:globe-americas" class="mr-2" style="transform: scale(1.5);" />
             Turismo
